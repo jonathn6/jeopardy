b4cc7c35a072f0dc4b643c4aee3c3f9d4ae65168
jonathn6 <jonathn6@icloud.com>
2021-11-23 19:15:15 -0500

    complete exercise 1 - 5

---
+++ Testing/Controllers/ProdutController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class ProdutController : Controller
+    {
+
+        private readonly IProductRepository repo;
+
+        public ProdutController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();
+            return View();
+        }
+
+    }
+}
